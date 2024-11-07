8ae0387afae7ae6372f4ab6baa3fd86f3b76fef2
Srujan Geedipalli <balasaisrujan273@gmail.com>
2024-11-07 15:21:03 +0530

    final update

---
+++ lib/shared/constants.dart
@@ -0,0 +1,9 @@
+import 'package:flutter/material.dart';
+
+class Constants {
+  static String appId = "1:131834443904:web:6091769ad8fc5cf8c40e65";
+  static String apiKey = "AIzaSyD7JCae2Hf3I9W6QNwhDi14zuMRGexps5U";
+  static String messagingSenderId = "131834443904";
+  static String projectId = "chatappfirebase-3e6ec";
+  final primaryColor = const Color(0xFFee7b64);
+}
